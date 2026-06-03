e8016b7dc804bb3814ec835bf17007610174022e
Larry Ruckman <ruckman@slac.stanford.edu>
2026-06-03 16:46:39 -0700

    updating submodule locks

--- shared/ruckus.tcl
+++ shared/ruckus.tcl
@@ -3,9 +3,9 @@ source $::env(RUCKUS_PROC_TCL)
 
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
-   if { [SubmoduleCheck {aes-stream-drivers}  {7.2.1} ] < 0 } {exit -1}
-   if { [SubmoduleCheck {ruckus}             {4.26.0} ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}               {2.70.0} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {aes-stream-drivers}  {7.3.0} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {ruckus}             {4.27.0} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}               {2.71.0} ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
